97bb59c448d0dda715ce31399e6bb206255fd811
andreapalu <andrea.palu@accenture.com>
2020-09-26 12:52:47 +0200

    stockService impl

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, VERSION } from "@angular/core";
 import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { StockService } from "./service/stock.service";
 
 @Component({
   selector: "my-app",
@@ -16,9 +17,17 @@ export class AppComponent {
     "http://query.yahooapis.com/v1/public/yql?format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=&q=select%20*%20from%20yahoo.finance.quotes%20where%20symbol=%22goog%22";
   private headers = new Headers({ "Content-Type": "application/json" });
 
-  testCommit;
+  stocks = [
+    { symbol: "TSLA", name: "Tesla, Inc." },
+    { symbol: "MSFT", name: "Microsoft Corporation" },
+    { symbol: "SPCE", name: "Virgin Galactic" },
+    { symbol: "CVS", name: "CVS Pharmacy" }
+  ];
 
-  constructor(private httpClient: HttpClient) {}
+  constructor(
+    private httpClient: HttpClient,
+    private stockService: StockService
+  ) {}
 
   fetchData() {
     let res: ResponseModel[];
@@ -39,11 +48,15 @@ export class AppComponent {
   }
 
   fetchStock() {
-    this.httpClient
-      .get(this.url)
-      .subscribe(response => {
-        console.log("Yahoo dice: "+JSON.stringify(response));
-      });
+    this.httpClient.get(this.url).subscribe(response => {
+      console.log("Yahoo dice: " + JSON.stringify(response));
+    });
+  }
+
+  public tryService() {
+    this.stockService.getStockInformation(this.stocks[0].symbol).subscribe(response => {
+      console.log("questo: "+JSON.stringify(response));
+    });
   }
 }
 
--- src/app/service/stock.service.ts
+++ src/app/service/stock.service.ts
@@ -1,9 +1,19 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { Observable } from "rxjs";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class StockService {
+  private api =
+    "https://query1.finance.yahoo.com/v8/finance/chart/{0}?region=US&lang=en-US&includePrePost=false&interval=2m&range=1d&.tsrc=finance";
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) {}
+
+  public getStockInformation(ticker): Observable<any> {
+    let api = this.api.replace("{0}", ticker);
+    return this.httpClient.get(api);
+  }
+
 }
